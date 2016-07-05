5978402d32a1493811bd37dff7e47330050aea39
Pranav Rajpurkar <rajpurkar@users.noreply.github.com>
2016-07-05 12:06:49 +0400

    Add starter file

---
+++ src/hello_world.c
@@ -0,0 +1,45 @@
+#include <pebble.h>
+
+static Window *s_window;
+static TextLayer *s_text_layer;
+
+static void init(void) {
+	// Create a window and get information about the window
+	s_window = window_create();
+  Layer *window_layer = window_get_root_layer(s_window);
+  GRect bounds = layer_get_bounds(window_layer);
+	
+  // Create a text layer and set the text
+	s_text_layer = text_layer_create(bounds);
+	text_layer_set_text(s_text_layer, "Hi, I'm a Pebble!");
+  
+  // Set the font and text alignment
+	text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+	text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+
+	// Add the text layer to the window
+	layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_text_layer));
+  
+  // Enable text flow and paging on the text layer, with a slight inset of 10, for round screens
+  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 10);
+
+	// Push the window, setting the window animation to 'true'
+	window_stack_push(s_window, true);
+	
+	// App Logging!
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+}
+
+static void deinit(void) {
+	// Destroy the text layer
+	text_layer_destroy(s_text_layer);
+	
+	// Destroy the window
+	window_destroy(s_window);
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
