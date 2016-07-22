10e1fccca809a2ba7cef3b2ab30429632bef806a
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-22 18:30:13 +0400

    Window shows steps today

--- src/windows/main_window.c
+++ src/windows/main_window.c
@@ -1,11 +1,13 @@
 #include "main_window.h"
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static TextLayer *title_layer;
+static TextLayer *subtitle_layer;
 
 static TextLayer* make_text_layer(GRect bounds, GFont font) {
   TextLayer *this = text_layer_create(bounds);
   text_layer_set_background_color(this, GColorClear);
+  text_layer_set_text_color(this, GColorBlack);
   text_layer_set_text_alignment(this, GTextAlignmentCenter);
   text_layer_set_font(this, font);
   return this;
@@ -15,21 +17,35 @@ static void window_load(Window * window) {
   // Get the bounds of the root layer
   Layer *root_layer  = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(root_layer);
+  float center = bounds.size.h /= 2;
 
   // Create a text layer, and set the text
-  s_text_layer = make_text_layer(bounds, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text(s_text_layer, "hi!");
+  int height = 30;
+  title_layer = make_text_layer(GRect(0, center - height, bounds.size.w, height), fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text(title_layer, "Pebble Fit");
+
+  subtitle_layer = make_text_layer(GRect(0, center, bounds.size.w, center + height), fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  text_layer_set_text(subtitle_layer, "A Better Today");
+
 
   // Add text layer to the window
-  layer_add_child(root_layer, text_layer_get_layer(s_text_layer));
-
-  // Enable text flow and paging on the text layer, with a slight inset of 10, for round screens
-  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 10);
+  layer_add_child(root_layer, text_layer_get_layer(title_layer));
+    layer_add_child(root_layer, text_layer_get_layer(subtitle_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+  text_layer_destroy(title_layer);
   window_destroy(s_window);
+}
+
+void main_window_update_steps(int steps) {
+  if(steps > 0) {
+    static char s_buffer[32];
+    snprintf(s_buffer, sizeof(s_buffer), "%d steps today", steps);
+    text_layer_set_text(subtitle_layer, s_buffer);
+  } else {
+    text_layer_set_text(subtitle_layer, "No steps today");
+  }
 }
 
 void main_window_push() {
--- src/windows/main_window.h
+++ src/windows/main_window.h
@@ -3,3 +3,5 @@
 #include <pebble.h>
 
 void main_window_push();
+
+void main_window_update_steps(int steps);
