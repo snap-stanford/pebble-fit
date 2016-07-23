f9cab5c2474157d7bba62d5d4b979c059a542bb4
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-23 08:30:43 +0400

    Add health service as module

---
+++ src/services/health.c
@@ -0,0 +1,19 @@
+static void health_handler(HealthEventType event, void *context) {
+  HealthMetric metric = HealthMetricStepCount;
+  HealthServiceAccessibilityMask result = 
+    health_service_metric_accessible(metric, time_start_of_today(), time(NULL));
+  int steps = 0;
+  if(result == HealthServiceAccessibilityMaskAvailable) {
+    steps = (int)health_service_sum_today(metric);
+  }
+  main_window_update_steps(steps);
+}
+
+
+void health_subscribe() {
+  if(!health_service_events_subscribe(health_handler, NULL)) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Health not available!");
+  }
+}
+
+  
---
+++ src/services/health.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <pebble.h>
+#include "../windows/main_window.h"
+
+void health_subscribe();
