78f059fd7ad8f6de48363dad97612e858bae5676
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-23 10:21:30 +0400

    Add services

--- src/main.c
+++ src/main.c
@@ -4,30 +4,23 @@
 #include "modules/steps.h"
 #include "modules/wakeup.h"
 
+#include "services/health.h"
+#include "services/connection.h"
+
 #include "windows/main_window.h"
 
 static void deinit(void) {
-  set_wakeup_in_x();
-}
-
-static void health_handler(HealthEventType event, void *context) {
-  // Update step count
-  HealthMetric metric = HealthMetricStepCount;
-  HealthServiceAccessibilityMask result = 
-    health_service_metric_accessible(metric, time_start_of_today(), time(NULL));
-  int steps = 0;
-  if(result == HealthServiceAccessibilityMaskAvailable) {
-    steps = (int)health_service_sum_today(metric);
-  }
-  main_window_update_steps(steps);
+  wakeup_set();
 }
 
 static void init(void) {
   comm_init(APP_MESSAGE_INBOX_SIZE_MINIMUM, APP_MESSAGE_OUTBOX_SIZE_MINIMUM);
   main_window_push();
-  get_wakeup_reason();
-  if(!health_service_events_subscribe(health_handler, NULL)) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Health not available!");
+  if (wakeup_caused_launch()) {
+    main_window_remove();
+  } else {
+    health_subscribe();
+    connection_subscribe();
   }
 }
 
