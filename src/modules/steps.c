b3f6f87cce062bf35c16a4544c874260d9e68a70
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-24 10:03:08 +0400

    Simplify interface on steps module

--- src/modules/steps.c
+++ src/modules/steps.c
@@ -2,41 +2,43 @@
 
 #define MAX_ENTRIES 60
 static uint8_t s_data[MAX_ENTRIES];
-static time_t end;
+static int s_num_records;
+static time_t s_start;
 
-static int data_reload_steps(time_t * start, time_t * end) {
+static void load_data(time_t * start, time_t * end) {
   // Clear old data
+  s_num_records = 0;
   for(uint8_t i = 0; i < MAX_ENTRIES; i++) {
     s_data[i] = 0;
   }
 
   // Check data is available
-  int num_records = 0;
-  HealthServiceAccessibilityMask result = health_service_metric_accessible(HealthMetricStepCount, *start, *end);
-  if(result == HealthServiceAccessibilityMaskAvailable) {
-    // Read the data
-    HealthMinuteData minute_data[MAX_ENTRIES];
-    num_records = health_service_get_minute_history(&minute_data[0], MAX_ENTRIES, start, end);
+  HealthServiceAccessibilityMask result = 
+    health_service_metric_accessible(HealthMetricStepCount, *start, *end);
 
-    // Store it
-    for(int i = 0; i < num_records; i++) {
-      s_data[i] = minute_data[i].steps;
-    }
-
-    APP_LOG(APP_LOG_LEVEL_INFO, "Got %d/%d new entries from the Health API from %d to %d", (int)num_records, MAX_ENTRIES, (int) *start, (int) *end);
-
-  } else {
+  if(result != HealthServiceAccessibilityMaskAvailable) {
     APP_LOG(APP_LOG_LEVEL_INFO, "No data available from %d to %d!", (int) *start, (int) *end);
+    return;
   }
 
-  return (int) num_records;
+  // Read the data
+  HealthMinuteData minute_data[MAX_ENTRIES];
+  
+  s_num_records = health_service_get_minute_history(&minute_data[0], MAX_ENTRIES, start, end);
+  s_start = *start;
+  // Store it
+  for(int i = 0; i < s_num_records; i++) {
+    s_data[i] = minute_data[i].steps;
+  }
+
+  APP_LOG(APP_LOG_LEVEL_INFO, "Got %d/%d new entries from the Health API from %d to %d", (int)s_num_records, MAX_ENTRIES, (int) *start, (int) *end);
 }
 
-static void steps_sent_handler(DictionaryIterator *iter, void *context) {
+static void sent_handler(DictionaryIterator *iter, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Steps Sent from Watch!");
 }
 
-static void server_received_steps_handler(DictionaryIterator *iter, void *context) {
+static void received_server_handler(DictionaryIterator *iter, void *context) {
   if(dict_find(iter, AppKeyServerReceived)) {
     APP_LOG(APP_LOG_LEVEL_INFO, "Steps Received by Server!");
   }
@@ -44,36 +46,25 @@ static void server_received_steps_handler(DictionaryIterator *iter, void *contex
   cb();
 }
 
-static void send_steps_to_phone(time_t start, time_t end) {
-  int num_records = data_reload_steps(&start, &end);
+static void send_data_callback(DictionaryIterator * out) {
+  //write the data
+  dict_write_data(out, AppKeyStepsData, s_data, sizeof(uint8_t) * s_num_records);
+  dict_write_int(out, AppKeyStepsEndDate, &s_start, sizeof(int), true);
+}
 
-  if (num_records == 0) {
+static void send_to_phone(time_t start, time_t end) {
+  load_data(&start, &end);
+
+  if (s_num_records == 0) {
     APP_LOG(APP_LOG_LEVEL_INFO, "No new data");
     return;
   }
 
-  DictionaryIterator *out;
-
-  if(app_message_outbox_begin(&out) == APP_MSG_OK) {
-    app_message_register_outbox_sent(steps_sent_handler);
-    app_message_register_inbox_received(server_received_steps_handler);
-  
-    dict_write_data(out, AppKeyStepsData, s_data, sizeof(uint8_t) * num_records);
-    dict_write_int(out, AppKeyStepsEndDate, &start, sizeof(int), true);
-
-    if(app_message_outbox_send() != APP_MSG_OK) {
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Error starting send of message.");
-    }
-  } else {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Error beginning message");
-  }
+  comm_send_data(send_data_callback, sent_handler, received_server_handler);
 }
 
 void send_latest_steps_to_phone() {
-  if (end == 0) {
-    end = time(NULL) - (15 * SECONDS_PER_MINUTE);
-  }
-
+  time_t end = time(NULL) - (15 * SECONDS_PER_MINUTE);
   time_t start = end - (MAX_ENTRIES * SECONDS_PER_MINUTE);
-  send_steps_to_phone(start, end);
+  send_to_phone(start, end);
 }
