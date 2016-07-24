e754fbff62a675d0b76e6a1bcd98ef289df7641c
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-24 12:59:29 +0400

    Standard format file

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -5,47 +5,48 @@ var createGraph = function (param, legend) {
   g.setAttribute('id', param)
   g.setAttribute('class', 'graph')
   graphsDiv.append(g)
-  d3.json('/' + param+ '?watch=' + window.watch_token, function(data) {
-      for (var i = 0; i < data.length; i++) {
-        if($.isArray(data[i])) {
-          for (var j = 0; j < data[i].length; j++) {
-            var result = new Date(data[i][j].time)
-            result.setDate(result.getDate() + i) // add days
-            data[i][j].time = result
-          }
-        } else {
-          data[i].time = new Date(data[i].time)
+  d3.json('/' + param + '?watch=' + window.watch_token, function (data) {
+    for (var i = 0; i < data.length; i++) {
+      if ($.isArray(data[i])) {
+        for (var j = 0; j < data[i].length; j++) {
+          var result = new Date(data[i][j].time)
+          result.setDate(result.getDate() + i) // add days
+          data[i][j].time = result
         }
-      }
-      data = MG.convert.number(data, 'steps')
-
-      var options = {
-          title: param.replace(/_/g, ' '),
-          data: data,
-          width: $(g).width(),
-          height: 400,
-          right: 40,
-          target: '#' + param,
-          x_accessor: 'time',
-          y_accessor: 'steps',
-          interpolate: 'basic',
-      }
-
-      if($.isArray(data[0])) {
-        legendDiv = document.createElement('div') 
-        legendDiv.setAttribute('id', param+'legend')
-        $('#' + param).append(legendDiv)
-        options['legend'] = legend
-        options['legend_target'] = '#' + param + 'legend'
       } else {
-        options['color'] = '#43F436'
+        data[i].time = new Date(data[i].time)
       }
+    }
+    data = MG.convert.number(data, 'steps')
 
-      MG.data_graphic(options)
+    var options = {
+      title: param.replace(/_/g, ' '),
+      data: data,
+      width: $(g).width(),
+      animate_on_load: true,
+      height: 400,
+      right: 40,
+      target: '#' + param,
+      x_accessor: 'time',
+      y_accessor: 'steps',
+      interpolate: 'basic',
+    }
+
+    if ($.isArray(data[0])) {
+      legendDiv = document.createElement('div')
+      legendDiv.setAttribute('id', param + 'legend')
+      $('#' + param).append(legendDiv)
+      options['legend'] = legend
+      options['legend_target'] = '#' + param + 'legend'
+    } else {
+      options['color'] = '#43F436'
+    }
+
+    MG.data_graphic(options)
   })
 }
 
 var graphs = ['latest_hour', 'latest_day', 'last_3_days']
 for (var i = 0; i < graphs.length; i++) {
-    createGraph(graphs[i], ['today', 'yesterday', 'day_before_yesterday'])
+  createGraph(graphs[i], ['today', 'yesterday', 'day_before_yesterday'])
 }
