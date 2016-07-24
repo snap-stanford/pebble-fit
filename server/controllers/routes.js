942721d0ae47b351fc6d95a3ee3cade26cbde895
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-24 13:49:51 +0400

    Clean route interface

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,39 +1,43 @@
 var express = require('express')
 var router = express.Router()
+
+// controllers
 var query = require('./query')
 var main = require('./main')
 var activities = require('./activities')
 var events = require('./events')
-var _ = require('lodash')
 
-router.get('/steps', query.requireParam('query', ['data', 'watch', 'date']),
+router.get('/steps',
+  query.requireParam('query', ['data', 'watch', 'date']),
   function (req, res, next) {
-  activity.saveActivities(req.query.data.split(','), req.query.date, req.query.watch,
-    function (err) {
-    if (err) return next(err)
-    res.status(200).end()
+    activities.save(
+      req.query.data.split(','),
+      req.query.date,
+      req.query.watch,
+      function (err) {
+        if (err) return next(err)
+        res.status(200).end()
+      })
   })
-})
 
-router.get('/launch', query.requireParam('query', ['reason', 'watch', 'date']),
+router.get(
+  ['/launch', '/delaunch'],
+  query.requireParam('query', ['watch', 'date']),
   function (req, res, next) {
-  event.saveEvent('launch', req.query.reason, req.query.date, req.query.watch,
-    function (err) {
-    if (err) return next(err)
-    res.status(200).end()
+    events.save(
+      req.path.substr(1),
+      req.query.reason,
+      req.query.date,
+      req.query.watch,
+      function (err) {
+        if (err) return next(err)
+        res.status(200).end()
+      })
   })
-})
 
-router.get('/delaunch', query.requireParam('query', ['watch', 'date']),
-  function (req, res, next) {
-  event.saveEvent('delaunch', null, req.query.date, req.query.watch,
-    function (err) {
-    if (err) return next(err)
-    res.status(200).end()
-  })
-})
-
-router.get(['/latest_hour', '/latest_day', '/last_3_days'], query.requireParam('query', ['watch']),
+router.get(
+  ['/latest_hour', '/latest_day', '/last_3_days'],
+  query.requireParam('query', ['watch']),
   function (req, res, next) {
     main[req.path.substr(1)](req.query.watch, function (err, data) {
       if (err) return next(err)
@@ -41,16 +45,18 @@ router.get(['/latest_hour', '/latest_day', '/last_3_days'], query.requireParam('
     })
   })
 
-router.get('/analytics', query.requireParam('query', ['watch']), function (req, res, next) {
-  activities.get_last_activity_time(req.query.watch, function (err, last_time) {
-    if (err) return next(err)
-    res.render('index', {watch: req.query.watch, last_time: last_time})  
+router.get('/analytics',
+  query.requireParam('query', ['watch']),
+  function (req, res, next) {
+    activities.get_last_recorded_time(req.query.watch, function (err, last_time) {
+      if (err) return next(err)
+      res.render('index', {watch: req.query.watch, last_time: last_time})
+    })
   })
-})
 
-router.get('/', function (req, res, next) {
-  res.redirect('/analytics?watch=6147d09748dd323ff6d0a3cb50b593db')
-})
-
+router.get('/',
+  function (req, res, next) {
+    res.redirect('/analytics?watch=6147d09748dd323ff6d0a3cb50b593db')
+  })
 
 module.exports = router
