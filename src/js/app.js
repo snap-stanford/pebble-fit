1a9715dccbe8abd04f5b03bda695df954df6248d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-09-02 22:06:21 +0400

    Minimal level-based logging module

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,22 @@
-var DEBUG = true
-var server = 'http://192.168.1.2:3000'
+var log = require('./logging')
+log.set_level(2)
 
-function info (content) {
-  console.log(content)
-}
+// URL at which to send data
+var SERVER = 'http://192.168.1.2:3000'
 
-function debug (content) {
-  if (DEBUG) info(content)
-}
+// Flag to switch off server communication
+var USE_OFFLINE = true
 
+// Start by sending message to watch that ready.
 Pebble.addEventListener('ready', function () {
-  info('PebbleKit JS ready!')
+  log.info('PebbleKit JS ready!')
   Pebble.sendAppMessage({
     'AppKeyJSReady': 1
   })
 })
 
-function send_data_to_url (url) {
-  function ajax (url, type, callback) {
+function send_data_to_route (route) {
+  function send_request (url, type, callback) {
     var xhr = new XMLHttpRequest()
     xhr.onload = function () {
       callback(null, this.status, this.response, this.responseText)
@@ -29,53 +28,60 @@ function send_data_to_url (url) {
     xhr.send()
   }
 
-  ajax(url, 'GET', function (err, status) {
-    if (err || status !== 200) {
-      info(err || status)
-    } else {
-      Pebble.sendAppMessage({
-        'AppKeyServerReceived': 1
-      })
-    }
-  })
+  function send_received_message () {
+    Pebble.sendAppMessage({
+      'AppKeyServerReceived': 1
+    })
+  }
+
+  if (USE_OFFLINE) {
+    send_request(SERVER + route, 'GET', function (err, status) {
+      if (err || status !== 200) {
+        log.info(err || status)
+      } else {
+        send_received_message()
+      }
+    })
+  } else {
+    send_received_message()
+  }
 }
 
 function send_steps_data (data, date) {
-  debug('Uploading steps data...')
+  log.debug('Uploading steps data...')
   // Convert to string
   var str = '' + data[0]
   for (var i = 1; i < data.length; i++) {
     str += ',' + data[i]
   }
-  var url = server + '/steps' +
+  var url = '/steps' +
   '?date=' + date +
   '&data=' + str +
   '&watch=' + Pebble.getWatchToken()
 
-  send_data_to_url(url)
+  send_data_to_route(url)
 }
 
 function send_launch_data (reason, date) {
-  debug('Uploading launch data...')
-  var url = server + '/launch' +
+  log.debug('Uploading launch data...')
+  var url = '/launch' +
   '?date=' + date +
   '&reason=' + reason +
   '&watch=' + Pebble.getWatchToken()
 
-  send_data_to_url(url)
+  send_data_to_route(url)
 }
 
 function send_delaunch_data (date) {
-  debug('Uploading delaunch data...')
-  var url = server + '/delaunch' +
+  log.debug('Uploading delaunch data...')
+  var url = '/delaunch' +
   '?date=' + date +
   '&watch=' + Pebble.getWatchToken()
 
-  send_data_to_url(url)
+  send_data_to_route(url)
 }
 
 Pebble.addEventListener('appmessage', function (dict) {
-
   function load_data_array () {
     var length = dict.payload['AppKeyArrayLength']
     var start = dict.payload['AppKeyArrayStart']
@@ -86,29 +92,29 @@ Pebble.addEventListener('appmessage', function (dict) {
     return data
   }
 
-  debug('Got appmessage: ' + JSON.stringify(dict.payload))
+  log.debug('Got appmessage: ' + JSON.stringify(dict.payload))
 
   var date
 
   if (dict.payload['AppKeyDate'] !== undefined) {
     date = dict.payload['AppKeyDate']
-    debug('Date: ' + date)
+    log.debug('Date: ' + date)
   }
 
   if (dict.payload['AppKeyStepsData'] !== undefined) {
     var data = load_data_array()
-    debug('Data: ' + data)
+    log.debug('Data: ' + data)
     send_steps_data(data, date)
   }
 
   if (dict.payload['AppKeyLaunchReason'] !== undefined) {
     var reason = dict.payload['AppKeyLaunchReason']
-    debug('Reason: ' + reason)
+    log.debug('Reason: ' + reason)
     send_launch_data(reason, date)
   }
 
   if (dict.payload['AppKeyDelaunchReason'] !== undefined) {
-    debug('Delaunched')
+    log.debug('Delaunched')
     send_delaunch_data(date)
   }
 })
---
+++ src/js/logging.js
@@ -0,0 +1,25 @@
+// Minimal level based logging
+var LOGGING_LEVEL = 2
+
+var info = function (content) {
+  if (LOGGING_LEVEL > 2) console.log(content)
+}
+
+var debug = function (content) {
+  if (LOGGING_LEVEL > 1) console.log(content)
+}
+
+var error = function (content) {
+  if (LOGGING_LEVEL > 0) console.log(content)
+}
+
+var set_level = function (level) {
+  LOGGING_LEVEL = level
+}
+
+module.exports = {
+  debug: debug,
+  info: info,
+  error: error,
+  set_level: set_level
+}
