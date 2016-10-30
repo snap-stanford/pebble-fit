1570d82702579f7c5d8b6550df05c71e93d13688
deanyxu <yingranxu@gmail.com>
2016-10-29 17:56:55 -0700

    Move data-backbone and pebble-nts to the develop branch

--- pebble-nts/old/worker_src/fourier.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <pebble_worker.h>
-
-void fft_2radix_real(int16_t *d, int16_t dlenpwr);
--- pebble-nts/src/demo_screens.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <pebble.h>
-#include "pinteract/pinteract.h"
-#include "constants.h"
-#include "display/display.h"
-#include "modules/comm.h"
-
-void demo_screens_open();
