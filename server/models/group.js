0b2e94e3ca744d3c09066cae5a7827dcafa70b43
deanyxu <yingranxu@gmail.com>
2017-06-03 10:45:00 -0700

    Detach server.

--- server/models/group.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var groupSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  file: {type: String, required: true},
-  configUpdatedAt: {type: Date, required: true},
-});
-
-module.exports = mongoose.model('Group', groupSchema);
--- server/models/message.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var messageSchema = new mongoose.Schema({
-  watch: {type: String, required: true},
-  messages: {type: Object, required: true},
-  created: {type: Date, default: Date.now},
-});
-
-module.exports = mongoose.model('Message', messageSchema);
